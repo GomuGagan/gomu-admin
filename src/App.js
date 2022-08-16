34956b5251b0a8d4d80cf182bdc970a9d2a5f743
GomuGagan <gagangomu@gmail.com>
2022-08-16 19:50:14 +0530

    today work

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React from 'react'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import "./app.css";
 import Topbar from './components/topbar/Topbar';
 import Sidebar from "./components/sidebar/Sidebar";
 import Home from "./pages/home/Home";
+import UserList from "./pages/userList/UserList"
 
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
       <Topbar/>
       <div className='container'>
         <Sidebar/>
-        <Home/>
+        {/* <Routes> */}
+          {/* <Route path="/" element={<Home />}> */}
+          <Home/>
+          {/* </Route> */}
+          {/* <Route path="/users"> */}
+          {/* <UserList/> */}
+          {/* </Route> */}
+        {/* </Routes> */}
+        
       </div>
       
-      </div>
+      </BrowserRouter>
   )
 }
 
